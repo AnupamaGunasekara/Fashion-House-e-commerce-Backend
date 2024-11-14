844594da19fa53ea07c40cfcf7a2f3234cfaf3b6
Anuradha00Herath <herathanuradha03@gmail.com>
2024-11-14 20:34:48 +0530

    edit update user

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin:"http://localhost:5173/",
-    Credentials: true
+    origin:"http://localhost:5173",
+    credentials: true
 }))
 
 // all routes
