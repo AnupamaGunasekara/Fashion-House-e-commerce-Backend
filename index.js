e083df5e5b4b5dba5da05e1940a3ef7e127bc9a1
Anuradha00Herath <herathanuradha03@gmail.com>
2024-11-17 01:19:47 +0530

    create products route

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const app = express()
-require('dotenv').config()
-const cookieParser = require('cookie-parser')
-const bodyParser = require('body-parser')
-const port = process.env.PORT || 3000
-
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const port = process.env.PORT || 3000;
 
 app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ limit: "25mb" }));
@@ -14,30 +13,33 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials: true
-}))
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 // all routes
-const authRoutes = require('./src/users/user.route');
+const authRoutes = require("./src/users/user.route");
+const productRoutes = require('./src/products/products.route');
 
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
+app.use('/api/products', productRoutes);
 
-
-main().then(()=> console.log("mongo db successfully conected.")).catch(err => console.log(err));
+main()
+  .then(() => console.log("mongo db successfully conected."))
+  .catch((err) => console.log(err));
 
 async function main() {
   await mongoose.connect(process.env.DB_URL);
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-  app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+  app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
 }
 
-
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
